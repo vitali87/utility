b903b7ede3ba539c2eaad04cfa38b743de78a2bd
vitali87 <eheva87@gmail.com>
2022-01-09 17:33:01 +0000

    Refining the extract script

--- extract.sh
+++ extract.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+formats=(.tar.xz .tar.gz .tar.bz2 .tar .tgz .bz .bz2 .tbz .tbz2 .gz .zip .jar .Z .rar)
 function extract() {
-if [[ $1 == *.tar.xz ]] || [[ $1 == *.tar.gz ]] || [[ $1 == *.tar.bz2 ]] || [[ $1 == *.tar ]] || [[ $1 == *.tgz ]]; then 
-	tar -xvf $1
-elif [[ $1 == *.bz ]] || [[ $1 == *.bz2 ]] || [[ $1 == *.tbz ]] || [[ $1 == *.tbz2 ]]; then
-	bzip2 -d -k $1
-elif [[ $1 == *.gz ]]; then
-	gunzip $1
-elif [[ $1 == *.zip ]]; then
-       	unzip $1
-elif [[ $1 == *.jar ]]; then
-       	jar -xvf $1
-elif [[ $1 == *.Z ]]; then
-	zcat $1 | tar -xvf -
-elif [[ $1 == *.rar ]]; then
-	unrar e $1
-fi
+  if [[ "$1" == *"${formats[0]}" ]] ||
+    [[ "$1" == *"${formats[1]}" ]] ||
+    [[ "$1" == *"${formats[2]}" ]] ||
+    [[ "$1" == *"${formats[3]}" ]] ||
+    [[ "$1" == *"${formats[4]}" ]]; then
+    tar -xvf "$1"
+  elif [[ "$1" == *"${formats[5]}" ]] ||
+    [[ "$1" == *"${formats[6]}" ]] ||
+    [[ "$1" == *"${formats[7]}" ]] ||
+    [[ "$1" == *"${formats[8]}" ]]; then
+    bzip2 -d -k "$1"
+  elif [[ "$1" == *"${formats[9]}" ]]; then
+    gunzip "$1"
+  elif [[ "$1" == *"${formats[10]}" ]]; then
+    unzip "$1"
+  elif [[ "$1" == *"${formats[11]}" ]]; then
+    jar -xvf "$1"
+  elif [[ "$1" == *"${formats[12]}" ]]; then
+    zcat "$1" | tar -xvf -
+  elif [[ "$1" == *"${formats[13]}" ]]; then
+    unrar e "$1"
+  else
+    echo "Please specify a correct archive format: "${formats[@]}""
+  fi
 }
-
