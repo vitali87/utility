db9338079ba256add24c2565cc990a41a7369422
Vitali Avagyan <eheva87@gmail.com>
2023-03-18 15:20:25 +0000

    refactor: reformat extract function and add error handling

--- utility.sh
+++ utility.sh
@@ -1,58 +1,53 @@
 #!/bin/bash
 
 formats=(.tar.xz .tar.gz .tar.bz2 .tar .tgz .bz .bz2 .tbz .tbz2 .gz .zip .jar .Z .rar .7z .tar.lzma .xz .lzma)
+
 extract() {
   second=${2:-"."}
-  if
-    [[ "$1" == *"${formats[0]}" ]] \
-      || [[ "$1" == *"${formats[1]}" ]] \
-      || [[ "$1" == *"${formats[2]}" ]] \
-      || [[ "$1" == *"${formats[3]}" ]] \
-      || [[ "$1" == *"${formats[4]}" ]]
-  then
-    tar -xvf "$1" -C "$second"
-  elif
-    [[ "$1" == *"${formats[5]}" ]] \
-      || [[ "$1" == *"${formats[6]}" ]] \
-      || [[ "$1" == *"${formats[7]}" ]] \
-      || [[ "$1" == *"${formats[8]}" ]]
-  then
-    bzip2 -d -k "$1"
-  elif
-    [[ "$1" == *"${formats[9]}" ]]
-  then
-    gunzip "$1" -c > "$second"
-  elif
-    [[ "$1" == *"${formats[10]}" ]] \
-      || [[ "$1" == *"${formats[11]}" ]]
-  then
-    unzip "$1" -d "$second"
-  elif
-    [[ "$1" == *"${formats[12]}" ]]
-  then
-    zcat "$1" | tar -xvf - -C "$second"
-  elif
-    [[ "$1" == *"${formats[13]}" ]]
-  then
-    rar x "$1" "$second"
-  elif
-    [[ "$1" == *"${formats[14]}" ]]
-  then
-    7z x "$1" "-o$second"
-  elif
-    [[ "$1" == *"${formats[15]}" ]]
-  then
-        tar -xf "$1" -C "$second" --lzma
-  elif
-    [[ "$1" == *"${formats[16]}" ]]
-  then
-    unxz "$1" -C "$second"
-  elif
-    [[ "$1" == *"${formats[17]}" ]]
-  then
-    unlzma "$1" -C "$second"
-  else
+  lowercase_filename=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+  matched=false
+
+  for format in "${formats[@]}"; do
+    if [[ "$lowercase_filename" == *"$format" ]]; then
+      matched=true
+      break
+    fi
+  done
+
+  if ! $matched; then
     echo "Please specify a correct archive format: \"${formats[*]}\""
+    return 1
+  fi
+
+  case "$lowercase_filename" in
+    *.tar.xz|*.tar.gz|*.tar.bz2|*.tar|*.tgz)
+      tar -xvf "$1" -C "$second" ;;
+    *.bz|*.bz2|*.tbz|*.tbz2)
+      bzip2 -d -k "$1" ;;
+    *.gz)
+      gunzip "$1" -c > "$second" ;;
+    *.zip|*.jar)
+      unzip "$1" -d "$second" ;;
+    *.z)
+      zcat "$1" | tar -xvf - -C "$second" ;;
+    *.rar)
+      rar x "$1" "$second" ;;
+    *.7z)
+      7z x "$1" "-o$second" ;;
+    *.tar.lzma)
+      tar -xf "$1" -C "$second" --lzma ;;
+    *.xz)
+      unxz "$1" -c > "$second" ;;
+    *.lzma)
+      unlzma "$1" -c > "$second" ;;
+    *)
+      echo "Unknown error occurred."
+      return 1 ;;
+  esac
+
+  if [ $? -ne 0 ]; then
+    echo "An error occurred while extracting the archive."
+    return 1
   fi
 }
 
