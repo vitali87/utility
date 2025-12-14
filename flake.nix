16ebe4ae8b701b9ce10694473e5ace2263fa238c
vitali87 <eheva87@gmail.com>
2025-12-14 00:54:46 +0000

    feat: prepare nix setup

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+    description = "Universal 7 - an intuitive cli for a human and AI";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }: {
+        # We'll fill this later
+    };
+}
