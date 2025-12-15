8bed9339488b0b3b39842110264ae5abf2ca1c24
vitali87 <eheva87@gmail.com>
2025-12-15 22:33:04 +0000

    feat: add other platforms

--- flake.nix
+++ flake.nix
@@ -3,52 +3,55 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs }:
-        let 
-            system = "aarch64-darwin";
-            pkgs = nixpkgs.legacyPackages.${system};
-        in 
-        {
-            devShells.${system}.default = pkgs.mkShell {
-                buildInputs = [
-                    pkgs.curl
-                    pkgs.jq
-                    pkgs.ripgrep
-                    pkgs.fd
-                    pkgs.qsv
+    outputs = { self, nixpkgs, flake-utils}:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+                let 
+                    pkgs = nixpkgs.legacyPackages.${system};
+                in 
+                {
+                    devShells.default = pkgs.mkShell {
+                        buildInputs = [
+                            pkgs.curl
+                            pkgs.jq
+                            pkgs.ripgrep
+                            pkgs.fd
+                            pkgs.qsv
 
-                    pkgs.coreutils
-                    pkgs.gnused
-                    pkgs.gawk
-                    pkgs.findutils
-                    pkgs.gnugrep
+                            pkgs.coreutils
+                            pkgs.gnused
+                            pkgs.gawk
+                            pkgs.findutils
+                            pkgs.gnugrep
 
-                    pkgs.bc
-                    pkgs.ffmpeg
-                    pkgs.imagemagick
-                    pkgs.gnutar
-                    pkgs.gzip
-                    pkgs.bzip2
-                    pkgs.xz
-                    pkgs.p7zip
-                    pkgs.unzip
+                            pkgs.bc
+                            pkgs.ffmpeg
+                            pkgs.imagemagick
+                            pkgs.gnutar
+                            pkgs.gzip
+                            pkgs.bzip2
+                            pkgs.xz
+                            pkgs.p7zip
+                            pkgs.unzip
 
-                    pkgs.rsync
+                            pkgs.rsync
 
-                    pkgs.openssl
-                    pkgs.yq-go
-                    pkgs.git
-                    pkgs.bash-completion
-                ];
+                            pkgs.openssl
+                            pkgs.yq-go
+                            pkgs.git
+                            pkgs.bash-completion
+                        ];
 
-                shellHook = ''
-                    source ${pkgs.bash-completion}/etc/profile.d/bash_completion.sh
-                    source ./utility.sh
-                    echo "u7 - Universal 7 CLI"
-                    echo "Verbs: show make drop convert move set run"
-                '';
-            };
-        };
+                        shellHook = ''
+                            source ${pkgs.bash-completion}/etc/profile.d/bash_completion.sh
+                            source ./utility.sh
+                            echo "u7 - Universal 7 CLI"
+                            echo "Verbs: show make drop convert move set run"
+                        '';
+                    };
+                }
+        );
 }
